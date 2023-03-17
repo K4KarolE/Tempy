8f7250f085a04f5e42da6404f000a58d615b194d
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-03-17 06:25:43 +0000

    get city list, 5 days forecast

--- functions/api.py
+++ functions/api.py
@@ -17,7 +17,6 @@ from pathlib import Path
 import settings
 
 
-
 functions_directory = os.path.dirname(__file__)     # = D:\_DEV\Python\MODDEC\functions   //in my case
 main_directory = functions_directory.replace("functions",'')
 
@@ -35,8 +34,7 @@ api_key = file.read()
 file.close()
 
 
-
-### FIND CITY
+## FIND CITY
 city = "Chelsea"
 limit = 5
 link_find_city = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={api_key}'
@@ -49,5 +47,26 @@ with open(path_json('find_city.json'), 'w') as f:
 f = open(path_json('find_city.json'))
 find_city_dic = json.load(f)
 
-
 # CITY LIST
+list_city_dic = {}
+for i in find_city_dic:
+    location_name = f"{i['name']}, {i['country']}, {i['state']}"
+    list_city_dic[location_name] = {'lat':i["lat"], 'lon':i["lon"]}
+
+# for i in list_city_dic:
+#     print(f'{i}: {list_city_dic[i]["lat"]}, {list_city_dic[i]["lon"]}')
+    # Chelsea, GB, England: 51.4875167, -0.1687007
+
+
+
+## FIVE DAY / 3 HOUR FORECAST
+lat = list_city_dic["Chelsea, GB, England"]["lat"]  # will come as chosen one from the city list / UI
+lon = list_city_dic["Chelsea, GB, England"]["lon"]
+
+link_five_day = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}'
+response = requests.get(link_five_day)
+
+# SAVE, LOAD 'FIVE DAY FORECAST' RESPONSE
+with open(path_json('5_day_forecast.json'), 'w') as f:
+    json.dump(response.json(), f, indent=2)
+
